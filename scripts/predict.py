750c99288df45aac7205011a4ac783446404f797
Tino Alvarez <arritmic@gmail.com>
2020-01-11 03:31:03 +0200

    Added RawTextHelpFormatter to the argument parser of predict script.

--- scripts/predict.py
+++ scripts/predict.py
@@ -4,23 +4,23 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 from config import Config, Model
 import argparse
+from argparse import RawTextHelpFormatter
 from predictors.svm_predictor import SVMPredictor
 import logging
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("ISO_DA")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='DialogueActTag - Tag a sentence with the ISO dialogue act taxonomy')
+    parser = argparse.ArgumentParser(description='DialogueActTag - Tag a sentence with the ISO dialogue act taxonomy',formatter_class=RawTextHelpFormatter)
     parser.add_argument('-model', dest='model', type=str,
                         help='the model folder to use for prediction')
     parser.add_argument('-class', dest='layer', type=str, default="all",
                         help='which level of the taxonomy to tag. Options are: \n'
-                             'all -- trains all the classifiers (default)'
-                             'dim -- trains the dimension classifier'
-                             'task -- trains the task CF classifier'
-                             'som -- trains the SOM CF classifier')
+                             'all -- trains all the classifiers (default) \n'
+                             'dim -- trains the dimension classifier \n'
+                             'task -- trains the task CF classifier \n'
+                             'som -- trains the SOM CF classifier \n')
     parser.add_argument('-s', dest='sentence', type=str, help="the sentence to tag")
     parser.add_argument('-p', dest='prev', type=str, help="[optional] the previous sentence in the dialogue")
 
@@ -38,3 +38,4 @@ if __name__ == "__main__":
         raise NotImplementedError(f"Unknown classifier type: {cfg.model_type}")
     logger.info("Tagging utterance")
     print(tagger.dialogue_act_tag(args.sentence))
+
